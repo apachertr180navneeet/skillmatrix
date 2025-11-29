97bfb649c068fbf1cc5cf43b3a09a6d9ad468780
apachertr180navneeet <navneet@brenin.co>
2025-11-29 10:30:52 +0530

    heading

--- resources/views/admin/layouts/login_layout.blade.php
+++ resources/views/admin/layouts/login_layout.blade.php
@@ -2,7 +2,7 @@
 <html lang="en" ng-app="{{ config('app.name') }}">
     <head>
         <meta charset="utf-8" />
-        <title>{{ config('app.name') }}</title>
+        <title>Skill Matrix</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta content="" />
         <meta name="csrf-token" content="{{ csrf_token() }}" />
