8f32fd842e3555027afafb766d237e8059121b81
apachertr180navneeet <navneet@brenin.co>
2025-12-13 10:44:31 +0530

    code update

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Web\HomeController;
 
 // Admin Controller
-use App\Http\Controllers\Admin\{
+use App\Http\Controllers\Super_Admin\{
     CompanyController,
     UserController,
     AdminAuthController,
